81b3ac5f8fd18f21b01be265d2d0370f3f7ce9ff
Anders Nelson <anders.f.nelson@gmail.com>
2021-10-27 08:56:38 -0500

    test files

---
+++ SphereTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SphereTest {
+    @Test
+    public void volumeTest() { 
+        //instantiate new circle object
+    Sphere s = new Sphere(); 
+
+    
+    //Set up result variable for the circumference calculation
+    double result = s.volume(5);
+        //Check that the result is within .01 of the expected
+    assertEquals(523.5988, result, 0.01);
+}
+}
